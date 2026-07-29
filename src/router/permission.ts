080d4ae5f13eb63769018cf248e6c69fdd7dec7a
ruiskey <18130319287@163.com>
2026-07-29 22:20:58 +0800

    梳理动态路由加载步骤router.addRoute('Layout', route)

--- src/router/permission.ts
+++ src/router/permission.ts
@@ -4,6 +4,12 @@ import {generateRoutes} from './routes'
 export function addDynamicRoutes(menuList:any[]){
     const routes = generateRoutes(menuList)
     routes.forEach(route=>{
+        // 所以所有业务页面：
+        // User.vue
+        // Project.vue
+        // Order.vue
+        // 都应该成为 Layout 的 children
+        // 把动态生成的业务页面路由，注册到后台布局 Layout 下面，让它们共享 Header、Sidebar，只替换 Layout 内部的 router-view。
         router.addRoute('Layout', route)
     })
 }
