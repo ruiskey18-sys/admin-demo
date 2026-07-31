41bda8fb0ba12641dfe4f574b4c88a7321e960bf
ruiskey <18130319287@163.com>
2026-07-31 16:26:04 +0800

    根据mock的menu内容修改Sidebar，支持扁平路由跳转

--- src/layout/components/Sidebar.vue
+++ src/layout/components/Sidebar.vue
@@ -9,19 +9,11 @@ const menuStore = useMenuStore()
 <template>
     <aside class="aside">
         <div class="logo">Ruiskey's Home</div>
-        <div v-for="menu in menuStore.menuList" :key="menu.id">
             <!-- 一级菜单 -->
-            <div class="menu-title">
+            <router-link class="menu-item" v-for="menu in menuStore.menuList" :key="menu.path" :to="menu.path">
                 <span>{{ menu.icon }}</span>
                 {{  menu.title }}
-            </div>
-            
-            <!-- 二级菜单 -->
-            <router-link v-for="value in menu.children" class="menu-item" :key="value.id" :to="value.path">
-                {{  value.title }}
-            </router-link>
-        </div>
-        
+            </router-link>      
     </aside>
 
 </template>
